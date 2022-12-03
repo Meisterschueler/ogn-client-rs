36a7ee12e4a517830252f8051903cfaa4bbeb8f9
Meisterschueler <kammermark@gmx.de>
2022-12-03 13:44:46 +0100

    added optional parameter '--source'

--- src/console_logger.rs
+++ src/console_logger.rs
@@ -6,17 +6,14 @@ extern crate json_patch;
 
 use actix::*;
 use actix_ogn::OGNMessage;
-use aprs_parser::{AprsData, AprsPosition};
-use influxdb_line_protocol::{DataPoint, FieldValue};
-use json_patch::merge;
 use log::{debug, error, warn};
-use serde_json::json;
 use std::time::SystemTime;
 
-use crate::OutputFormat;
-use crate::OGNComment;
+use crate::{InputSource, OutputFormat, OGNMessageConverter};
+
 
 pub struct ConsoleLogger {
+    pub source: InputSource,
     pub format: OutputFormat,
     pub includes: Option<Vec<String>>,
     pub excludes: Option<Vec<String>>,
@@ -24,189 +21,6 @@ pub struct ConsoleLogger {
 
 impl Actor for ConsoleLogger {
     type Context = Context<Self>;
-}
-
-impl ConsoleLogger {
-    fn print_raw(&mut self, message: &OGNMessage) {
-        let ts = SystemTime::now()
-            .duration_since(SystemTime::UNIX_EPOCH)
-            .unwrap()
-            .as_nanos();
-
-        println!("{ts}: {aprs}", aprs = message.raw);
-    }
-
-    fn print_json(&mut self, message: &OGNMessage) {
-        match aprs_parser::parse(&message.raw) {
-            Ok(value) => {
-                let mut json_aprs = json!({
-                    "src_call": value.from.call,
-                    "dst_call": value.to.call,
-                    "receiver": value.via.iter().last().cloned().unwrap().call,
-                });
-                match value.data {
-                    aprs_parser::AprsData::Position(pos) => {
-                        let ogn_comment: OGNComment = pos.comment.as_str().into();
-                        let mut latitude: f64 = *pos.latitude as f64;
-                        let mut longitude: f64 = *pos.longitude as f64;
-                        if let Some(additional_precision) = ogn_comment.additional_precision {
-                            latitude += (additional_precision.lat as f64) / 1000.0;
-                            longitude += (additional_precision.lon as f64) / 1000.0;
-                        }
-
-                        let patch = json!({
-                            "latitude": latitude,
-                            "longitude": longitude,
-                            "symbol_table": pos.symbol_table,
-                            "symbol_code": pos.symbol_code,
-                        });
-
-                        merge(&mut json_aprs, &patch);
-                        
-                        if let Some(course) = ogn_comment.course {
-                            merge(&mut json_aprs, &json!({"course": course}));
-                        }
-                        if let Some(speed) = ogn_comment.speed {
-                            merge(&mut json_aprs, &json!({"speed": speed}));
-                        }
-                        if let Some(altitude) = ogn_comment.altitude {
-                            merge(&mut json_aprs, &json!({"altitude": altitude}));
-                        }
-                        if let Some(id) = ogn_comment.id {
-                            merge(&mut json_aprs, &json!({"address_type": id.address_type}));
-                            merge(&mut json_aprs, &json!({"aircraft_type": id.aircraft_type}));
-                            merge(&mut json_aprs, &json!({"is_stealth": id.is_stealth}));
-                            merge(&mut json_aprs, &json!({"is_notrack": id.is_notrack}));
-                            merge(&mut json_aprs, &json!({"address": id.address}));
-                        }
-                        if let Some(climb_rate) = ogn_comment.climb_rate {
-                            merge(&mut json_aprs, &json!({"climb_rate": climb_rate}));
-                        }
-                        if let Some(turn_rate) = ogn_comment.turn_rate {
-                            merge(&mut json_aprs, &json!({"turn_rate": turn_rate}));
-                        }
-                        if let Some(error) = ogn_comment.error {
-                            merge(&mut json_aprs, &json!({"error": error}));
-                        }
-                        if let Some(frequency_offset) = ogn_comment.frequency_offset {
-                            merge(&mut json_aprs, &json!({"frequency_offset": frequency_offset}));
-                        }
-                        if let Some(signal_quality) = ogn_comment.signal_quality {
-                            merge(&mut json_aprs, &json!({"signal_quality": signal_quality}));
-                        }
-                        let comment: &str = &ogn_comment.comment.unwrap_or_default();
-                        if comment != "" {
-                            merge(&mut json_aprs, &json!({"comment": comment}));
-                        }
-                    }
-                    aprs_parser::AprsData::Message(_) => {}
-                    _ => {}
-                };
-                println!("{}", json_aprs.to_string());
-            }
-            Err(err) => error!("Not a valid APRS message: {}", err),
-        }
-    }
-
-    fn print_influxdb(&mut self, message: &OGNMessage) {
-        let timestamp = SystemTime::now()
-            .duration_since(SystemTime::UNIX_EPOCH)
-            .unwrap()
-            .as_nanos();
-        match aprs_parser::parse(&message.raw) {
-            Ok(value) => {
-                let tags: Vec<(&str, &str)> = vec![
-                    ("src_call", &value.from.call),
-                    ("dst_call", &value.to.call),
-                    ("receiver", &value.via.iter().last().unwrap().call),
-                ];
-
-                if let AprsData::Position(pos) = value.data {
-                    let mut fields: Vec<(&str, FieldValue)> = vec![];
-                    
-                    let ogn_comment: OGNComment = pos.comment.as_str().into();
-                    let symbol_table: &str = &pos.symbol_table.to_string();
-                    let symbol_code: &str = &pos.symbol_code.to_string();
-                    let mut latitude: f64 = *pos.latitude as f64;
-                    let mut longitude: f64 = *pos.longitude as f64;
-                    if let Some(additional_precision) = ogn_comment.additional_precision {
-                        latitude += (additional_precision.lat as f64) / 1000.0;
-                        longitude += (additional_precision.lon as f64) / 1000.0;
-                    }
-
-                    fields.push(("latitude", FieldValue::Float(latitude)));
-                    fields.push(("longitude", FieldValue::Float(longitude)));
-                    fields.push(("symbol_table", FieldValue::String(symbol_table)));
-                    fields.push(("symbol_code", FieldValue::String(symbol_code)));
-
-                    if let Some(course) = ogn_comment.course {
-                        fields.push(("course", FieldValue::Float(course.into())));
-                    }
-                    if let Some(speed) = ogn_comment.speed {
-                        fields.push(("speed", FieldValue::Float(speed.into())));
-                    }
-                    if let Some(altitude) = ogn_comment.altitude {
-                        fields.push(("altitude", FieldValue::Float(altitude.into())));
-                    }
-                    if let Some(id) = ogn_comment.id {
-                        fields.push(("address_type", FieldValue::Integer(id.address_type.into())));
-                        fields.push(("aircraft_type", FieldValue::Integer(id.aircraft_type.into())));
-                        fields.push(("is_stealth", FieldValue::Boolean(id.is_stealth)));
-                        fields.push(("is_notrack", FieldValue::Boolean(id.is_notrack)));
-                        fields.push(("address", FieldValue::Integer(id.address.into())));
-                    }
-                    if let Some(climb_rate) = ogn_comment.climb_rate {
-                        fields.push(("climb_rate", FieldValue::Integer(climb_rate as i64)));
-                    }
-                    if let Some(turn_rate) = ogn_comment.turn_rate {
-                        fields.push(("turn_rate", FieldValue::Float(turn_rate as f64)));
-                    }
-                    if let Some(error) = ogn_comment.error {
-                        fields.push(("error", FieldValue::Integer(error as i64)));
-                    }
-                    if let Some(frequency_offset) = ogn_comment.frequency_offset {
-                        fields.push(("frequency_offset", FieldValue::Float(frequency_offset as f64)));
-                    }
-                    if let Some(signal_quality) = ogn_comment.signal_quality {
-                        fields.push(("signal_quality", FieldValue::Float(signal_quality as f64)));
-                    }
-                    let comment: &str = &ogn_comment.comment.unwrap_or_default();
-                    if comment != "" {
-                        fields.push(("comment", FieldValue::String(comment)));
-                    }
-                    
-                    let data_point = DataPoint {
-                        measurement: "ogn_position",
-                        tag_set: tags,
-                        field_set: fields,
-                        timestamp: Some(timestamp as i64),
-                    };
-                    let data = data_point.into_string().unwrap();
-                    print!("{data}");
-                } else {
-                    let data_point = DataPoint {
-                        measurement: "ogn_unparsed",
-                        tag_set: tags,
-                        field_set: vec![("message", FieldValue::String(&message.raw))],
-                        timestamp: Some(timestamp as i64),
-                    };
-                    let data = data_point.into_string().unwrap();
-                    print!("{data}");
-                }
-            }
-            Err(err) => {
-                let error_string = err.to_string();
-                let data_point = DataPoint {
-                    measurement: "ogn_error",
-                    tag_set: vec![],
-                    field_set: vec![("error", FieldValue::String(&error_string)), ("message", FieldValue::String(&message.raw))],
-                    timestamp: Some(timestamp as i64),
-                };
-                let data = data_point.into_string().unwrap();
-                print!("{data}");
-            }
-        }
-    }
 }
 
 impl Handler<OGNMessage> for ConsoleLogger {
@@ -225,11 +39,16 @@ impl Handler<OGNMessage> for ConsoleLogger {
         };
 
         if passes_filter {
-            match self.format {
-                OutputFormat::Raw => self.print_raw(&message),
-                OutputFormat::Json => self.print_json(&message),
-                OutputFormat::Influx => self.print_influxdb(&message),
-            }
+            let timestamp = SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap()
+                .as_nanos();
+            let output_string =  match self.format {
+                OutputFormat::Raw => message.to_raw(timestamp),
+                OutputFormat::Json => message.to_json(timestamp),
+                OutputFormat::Influx => message.to_influx(timestamp),
+            };
+            println!("{output_string}");
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,23 @@ extern crate pretty_env_logger;
 
 mod console_logger;
 mod ogn_comment;
+mod ogn_message_converter;
+
+use std::io::BufRead;
 
 use actix::*;
-use actix_ogn::OGNActor;
+use actix_ogn::{OGNActor, OGNMessage};
 use clap::Parser;
 use console_logger::ConsoleLogger;
 use ogn_comment::OGNComment;
+use ogn_message_converter::OGNMessageConverter;
+
+#[derive(clap::ValueEnum, Clone, Debug)]
+pub enum InputSource {
+    Glidernet,
+    Console,
+}
+
 
 #[derive(clap::ValueEnum, Clone, Debug)]
 pub enum OutputFormat {
@@ -22,6 +33,10 @@ pub enum OutputFormat {
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about=None)]
 struct Cli {
+    /// specify input source
+    #[arg(short, long, value_enum, default_value_t = InputSource::Glidernet)]
+    source: InputSource,
+
     /// specify output format
     #[arg(short, long, value_enum, default_value_t = OutputFormat::Raw)]
     format: OutputFormat,
@@ -41,6 +56,7 @@ fn main() {
     // Get the command line arguments
     let cli = Cli::parse();
 
+    let source = cli.source;
     let format = cli.format;
 
     let includes = cli
@@ -51,19 +67,40 @@ fn main() {
         .excludes
         .and_then(|s| Some(s.split(',').map(|x| x.to_string()).collect::<Vec<_>>()));
 
-    // Start actix
-    let sys = actix::System::new("test");
+    match source {
+        InputSource::Console => {
+            for line in std::io::stdin().lock().lines() {
+                let line = line.unwrap();
+                let (first, second) = line.split_once(' ').unwrap();
+                
+                let ts = first[0..first.len()-1].parse::<u128>().unwrap();
+                let message = OGNMessage{raw: second.to_string()};
+                let result = match format {
+                    OutputFormat::Raw => message.to_raw(ts),
+                    OutputFormat::Json => message.to_json(ts),
+                    OutputFormat::Influx => message.to_influx(ts),
+                };
+                print!("{result}");
+            }
+        },
+        InputSource::Glidernet => {
+            // Start actix
+            let sys = actix::System::new("test");
 
-    // Start actor in separate threads
-    let console_logger: Addr<_> = ConsoleLogger {
-        format: format,
-        includes: includes,
-        excludes: excludes,
+            // Start actor in separate threads
+            let console_logger: Addr<_> = ConsoleLogger {
+                    source: source,
+                    format: format,
+                    includes: includes,
+                    excludes: excludes,
+                }
+                .start();
+
+            // Start OGN client in separate thread
+            let _addr: Addr<_> = Supervisor::start(move |_| OGNActor::new(console_logger.recipient()));
+
+            let _result = sys.run();
+        }
     }
-    .start();
-
-    // Start OGN client in separate thread
-    let _addr: Addr<_> = Supervisor::start(move |_| OGNActor::new(console_logger.recipient()));
-
-    let _result = sys.run();
+    
 }
